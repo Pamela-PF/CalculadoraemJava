b230b308b737b59948d894c7460ecf57dcade71e
Pamela Francisco <ni.pan1010@gmail.com>
2022-02-18 15:16:03 -0300

    public calcularSoma criado

--- calculadora.java
+++ calculadora.java
@@ -1,13 +1,32 @@
 public class calculadora{
-    int soma = a + b;
+    int soma;
     int sub;
+    int a;
+    int b;
 
+    public int calcularSoma(){
+        soma = a + b;
+        return soma;
+    }
 
     public int getSoma() {
         return soma;
     }
     public void setSoma(int soma) {
         this.soma = soma;
+    }
+
+    public int getSub() {
+        return sub;
+    }
+
+    public void setSub(int sub) {
+        this.sub = sub;
+    }
+
+    public int calcularSub(){
+        sub = a - b;
+        return sub;
     }
 
 

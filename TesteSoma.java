8c34ab95133263b7f2de4f58ae47759b5f630703
Pamela Francisco <ni.pan1010@gmail.com>
2022-02-18 14:58:31 -0300

    Teste soma criado

---
+++ TesteSoma.java
@@ -0,0 +1,7 @@
+public class TesteSoma {
+    int a = 2;
+    int b = 5;
+
+    System.out.print
+
+}
---
+++ calculadora.java
@@ -0,0 +1,14 @@
+public class calculadora{
+    int soma = a + b;
+    int sub;
+
+
+    public int getSoma() {
+        return soma;
+    }
+    public void setSoma(int soma) {
+        this.soma = soma;
+    }
+
+
+}
